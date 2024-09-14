1b6c7e08f1bf3d9da4559d4d2eec787a1986e846
Nikhil kumar <131590056+nikhil7591@users.noreply.github.com>
2024-09-14 07:27:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -108,4 +108,4 @@ def contact_us():
     return render_template('contactus.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=env.PROCESS.PORT||8080, debug=True)
